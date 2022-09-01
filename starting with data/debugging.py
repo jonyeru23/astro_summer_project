f0a830b5a219111dfb12fb16a7f3f5deb5bac0ee
jonathanyeru <jonathanyerushalmi36@gmail.com>
2022-09-01 09:47:38 +0300

    i think we got it

--- starting with data/debugging.py
+++ starting with data/debugging.py
@@ -1,96 +1,161 @@
+## remember to add extinction to their data!!!
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from astropy import units as u
 from astropy import constants as const
 import astropy
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+import pysynphot as S
+import emcee
 
-distance = 26.4 * 10**6 * u.pc
-
-R_e = {3/2: 3.07 * 10**12 * u.cm, 3: 4.57 * 10**12 * u.cm}
-
-v_e = {3/2: 2.47 * 10**9 * u.cm / u.s, 3: 2.69 * 10**9 * u.cm / u.s}
-
-M_e = {3/2: 3.99 * 10**-2 * u.solMass, 3: 38.34 * 10**-2 * u.solMass}
-
-## we set M_c = M_sun becuase the dependence is very week
-M_c = 1 * u.solMass
-
-## M = Me + Mc
-M = lambda n: M_e[n] + M_c
-
-## vs,8.5 is the velocity of the shock vs in units of 10^8.5 cm s−1
-v = lambda n: v_e[n] / (10**8.5)
-
-## where R13 = Re/10^13 cm
-R_13 = lambda n: R_e[n] / (10**13)
-
-## where k0.34 is the opacity in units of 0.34 cm2 g−1
-## where k0.34 is the opacity in units of 0.34 cm2 g−1
-## k = 1 * 0.34 * u.cm**2 / u.g
 k = 1
+M_c = 1
+distance = 26.4 * 10**6 * u.pc
+# theta = [v85, R13, M_e, offset]
 
-
-def f_p(n):
+def f_p(n, M_e):
     """
     returns the ratio between the enitial mass and the core mass.
     """
     if n == 3 / 2:
-        return (M_e[n] / M_c) ** 0.5
+        return (M_e / M_c) ** 0.5
     elif n == 3:
-        return 0.08 * (M_e[n] / M_c)
+        return 0.08 * (M_e / M_c)
 
 
-def L(n, t):
+def L(n, t, theta):
     """
-    the final luminosity function as SW16, t in days
+    t - days, M_e - solMass, v85 - vs * 10**-8.5 * cm * s**-1, R13 - R_e * 10**-13 * cm
     """
+    v85, R13, M_e, offset = theta
+    t_offset = t - offset
     if n == 3 / 2:
         nums = [1.88, -0.086, 1.67, 0.8]
     else:
         nums = [1.66, -0.175, 4.57, 0.73]
-
     A = nums[0] * 10 ** 42
-    B = (v(n) / (f_p(n) * M(n) * k)).value
-    C = (v(n) ** 2 * (R_13(n) / k)).value
-    D = (nums[2] / np.sqrt(19.5 * k * M_e[n] * v(n) ** -1)).value
-    print(A, B, C, D)
-    final = A * ((B * t ** 2) ** nums[1]) * C * np.exp(-(D * t) ** nums[3]) * u.erg / u.s
-    final = final.value
-    return final
+    B = (v85 / (f_p(n, M_e) * (M_e + M_c) * k))
+    C = (v85 ** 2 * (R13 / k))
+    D = (nums[2] / (19.5 * np.sqrt(k * M_e * v85 ** -1)))
+
+    return A * ((B * t_offset ** 2) ** nums[1]) * C * np.exp(-(D * t_offset) ** nums[3]) * u.erg / u.s
 
 
-def L_p15(t):
+def T(n, t, theta):
     """
-    t should be in seconds
+    the color temp is given by this function.
+    where t is in days and M is in solar masses.
     """
-    k = 1
-    R_13 = 4.00 / 10 * u.cm
-    v_9 = 2.05 * u.cm / u.s
-    M_e = 2.5 * 10 ** -2 * u.solMass
-    M_sun = 1 * u.solMass
-
-    t = (t * u.d).to(u.s)
-
-    A = 8.27 * 10 ** 42 * k ** -1 * v_9 ** 2 * R_13
-    B = -4.135 * 10 ** -11 * t * (t * v_9 + 2 * 10 ** 4 * R_13)
-    C = k ** -1 * (M_e / (0.01 * M_sun)) ** -1
-    return A.value * np.exp(B.value * C.value)
+    v85, R13, M_e, offset = theta
+    t_offset = t - offset
+    print(t, t_offset)
+    if n == 3 / 2:
+        nums = [2.05, 0.027]
+    else:
+        nums = [1.96, 0.016]
+    T = nums[0] * 10 ** 4 * (((v85 * t_offset) ** 2) / (f_p(n, M_e) * (M_e + M_c) * k)) ** nums[1] * (
+                R13 / k) ** 0.25 * t_offset ** -0.5
+    return T * u.K
 
 
-def main():
-    time = np.linspace(0.001, 3.5)
-    n = 3
-    plt.plot(time, L(n, time), '--k', label='n=3')
+def R(n, t, theta):
+    """
+    gets t in days, then converts the result to rsun
+    R^2 = sqrt(L(t)/4pi sigma T^4)
+    """
+    sigma_sb = const.sigma_sb.to(u.erg * u.cm ** -2 * u.s ** -1 * u.K ** -4)
+    return np.sqrt(L(n, t, theta) / (4 * np.pi * sigma_sb * T(n, t, theta) ** 4)).to(u.solRad)
 
-    n = 3 / 2
-    plt.plot(time, L(n, time), 'k', label='n=3/2')
 
-    plt.plot(time, L_p15(time), '-r', label='p15')
-    plt.legend(fontsize=14)
-    plt.title("L to time")
-    plt.yscale('log')
-    plt.show()
+def get_mag(n, t, theta):
+    mag = []
+    for temp in T(n, t, theta).value:
+        bb = S.BlackBody(temp)
 
-if __name__ == '__main__':
-    main()
+        bp = S.ObsBandpass('v')
+
+        obs = S.Observation(bb, bp)
+
+        mag.append(obs.effstim('VegaMag'))
+    return np.array(mag) - 2.5 * np.log10(((R(n, t, theta) / (1 * u.solRad)) ** 2) * ((1000.0 * u.pc / distance) ** 2))
+
+
+def log_prior(theta):
+    """
+    i used the values Iair gave me, plus the times we expect the explosion to occur (by their paper)
+    """
+    v85, R13, M_e, offset = theta
+    R_e = R13 * 1e13
+    v_e = v85 * 10 ** 8.5
+    if R_e > 1e10 and R_e < 1e14 and v_e > 100e5 and v_e < 100000e5 and M_e > 0.005 and M_e < 1 and \
+            offset > 0.70342593 and offset < 0.73376157:
+        return 0  # log(1)
+    else:
+        return -np.inf  # log(0)
+
+
+def log_likelihood(n, theta, t, meas_mag, meas_mag_err):
+    meas_flux = mag_to_flux(meas_mag)
+    meas_flux_err = mag_to_fluxerr(meas_mag, meas_mag_err)
+
+    expected_flux = mag_to_flux(get_mag(n, t, theta))
+
+    chi2 = get_chi2(meas_flux, meas_flux_err, expected_flux)
+
+    normalization = get_normalization(meas_flux)
+
+    return -0.5 * np.sum(normalization + chi2)
+
+
+def mag_to_flux(mag):
+    return 10 ** (-0.4 * mag)
+
+
+def mag_to_fluxerr(mag, mag_err):
+    return abs(mag_to_flux(mag) * (-0.4) * np.log(10) * mag_err)
+
+
+def get_chi2(meas, meas_err, expected):
+    return ((meas - expected) / meas_err) ** 2
+
+
+def get_normalization(meas):
+    return np.log(2 * np.pi * meas ** 2)
+
+
+def log_postirior(theta, n, t, meas_mag, meas_mag_err):
+    lp = log_prior(theta)
+    if not np.isfinite(lp):
+        return -np.inf
+    return lp + log_likelihood(n, theta, t, meas_mag, meas_mag_err)
+
+def set_walkers():
+    """
+    # initialize walkers -  theta = [v85, R13, M_e, offset]
+    """
+    initial_guess = [1.7, 0.4, 0.02, 0.715]
+    initial_spread = [0.2, 0.2, 0.002, 0.01]
+
+    return np.random.randn(32, 4) * initial_spread + initial_guess
+
+
+
+path = r"C:\Users\User\OneDrive - mail.tau.ac.il\Desktop\אוניברסיטה\אסטרו נודר\פרויקט קיץ\התחלה של קוד\astro_summer_project\starting with data\excel files\combined_data.xlsx"
+
+data = pd.read_excel(path, sheet_name='after extinction')
+
+t = np.array(data.loc[:, 'JD - 2457651.0[day]'])
+meas_mag = np.array(data.loc[:, 'V[mag]'])
+meas_mag_err = np.array(data.loc[:, 'error_V[mag]'])
+
+
+
+
+n = 3
+initial_pos = set_walkers()
+nwalkers, ndim = initial_pos.shape
+
+sampler = emcee.EnsembleSampler(nwalkers, ndim, log_postirior,
+                                args=(n, t, meas_mag, meas_mag_err))
+sampler.run_mcmc(initial_pos, 5000, progress=True);
