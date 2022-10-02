a6781c04f4b5ae3f8b8a144da69187c56abe8a68
jonathanyeru <jonathanyerushalmi36@gmail.com>
2022-10-02 17:36:57 +0300

    almost over with everything, i need to get past this mag figure though

---
+++ starting with data/SWN16-Sushaman/L_T_R.py
@@ -0,0 +1,225 @@
+from astropy import units as u
+from astropy import constants as const
+import numpy as np
+import pysynphot as S
+
+h = const.h.to_value(u.erg / u.Hz)
+k = const.k_B.to_value(u.erg / u.K)
+distance = 26.4 * 10**6 * u.pc
+
+"""
+This file contains the following classes: Time, Luminosity, Temp, Radius
+consentrated into one place.
+"""
+
+
+class Time:
+    @staticmethod
+    def multi(theta, x, y, z) -> float:
+        R500, M15, E51, offset = theta
+        return (M15 ** x) * (R500 ** y) * (E51 ** z)
+
+    def t_0(self, theta) -> float:
+        """t_0 is converted to days"""
+        return (155 * self.multi(theta, 0.23, 1.39, -0.81) * u.s).to(u.d)
+
+    def t_s(self, theta) -> float:
+        """t_s in converted to days"""
+        return (3.6 * self.multi(theta, 0.44, 1.49, -0.56) * u.h).to(u.d)
+
+    def t_c(self, theta) -> float:
+        """t_c in days"""
+        return 3.2 * self.multi(theta, 0.97, 2.02, -1.19) * u.d
+
+    def t_rec(self, theta) -> float:
+        """t_rec in days"""
+        return 16.6 * self.multi(theta, 0.22, 0.76, -0.43) * u.d
+
+    @staticmethod
+    def t_rc(theta) -> float:
+        """t_rc in days"""
+        R = theta[0]
+        return ((R * 500 * u.solRad).to(u.meter) / const.c).to(u.d)
+
+
+class T(Time):
+    def col(self, theta, t, nu):
+        """nu is frequency"""
+        return self.obs(theta, t) * (h * nu / (3 * k * self.obs(theta, t))) ** 0.2
+
+    def obs(self, theta, t):
+        """eq 8 in Shusman"""
+        offset = theta[3]
+        t_offset = t - (offset * u.d)
+        T0 = 4.3 * 10 ** 5 * self.multi(theta, -0.17, -0.52, 0.35)
+        if t < self.t_0(theta):
+            return T0
+
+        elif self.t_0(theta) <= t < self.t_s(theta):
+            return T0 * (t_offset / self.t_0(theta)) ** (-0.45)
+
+        elif self.t_s(theta) <= t < self.t_c(theta):
+            return T0 * (self.t_s(theta) / self.t_0(theta)) ** (-0.45) * (t_offset / self.t_s(theta)) ** (-0.35)
+
+        elif self.t_c(theta) <= t < self.t_rec(theta):
+            return T0 * (self.t_s(theta) / self.t_0(theta)) ** (-0.45) * (self.t_c(theta) / self.t_s(theta)) ** (-0.35) * (
+                        t / self.t_c(theta)) ** (-0.6)
+
+        return 0
+
+
+class Wave:
+    def __init__(self, band_filter='V', system='VegaMag'):
+        self.band_filter = band_filter
+        self.system = system
+        self.ob = S.ObsBandpass(band_filter)
+        self.central_freq = self.length_to_frequency(self.ob.avgwave())
+
+    @staticmethod
+    def length_to_frequency(wave_length):
+        """
+        accepts wave in Angstrom and returns frequency in Hz
+        """
+        wave_length = (wave_length * u.angstrom).to_value(u.m)
+        return const.c.value / wave_length
+
+    def get_range_freq(self, steps):
+        """
+        ******************************ask Iair about the width of the frequency************************
+        """
+        start = self.length_to_frequency(self.ob.avgwave() + self.ob.equivwidth() / 2)
+        stop = self.length_to_frequency(self.ob.avgwave() - self.ob.equivwidth() / 2)
+        step_size = (stop - start) / steps
+        return np.arange(start, stop, step_size)
+
+
+class L(Time):
+    def __init__(self, steps):
+        self.T = T()
+        self.steps = steps
+
+    def light_travel_time(self, theta, t) -> float:
+        """eq 1 in Kozyreva paper"""
+        dx = self.t_rc(theta) / self.steps
+        return 2 / self.t_rc(theta) * \
+               np.trapz([self.ltt_integrant(theta, t, t_tag) for t_tag in np.linspace(t-self.t_rc(theta), t, self.steps)],
+                        dx=dx)
+
+    def ltt_integrant(self, theta, t, t_tag) -> float:
+        """the function inside the integral"""
+        return self.broken_power_law(theta, t_tag) * (1 - (t - t_tag) / self.t_rc(theta))
+
+    def broken_power_law(self, theta, t) -> float:
+        """eq 7"""
+        offset = theta[3]
+        t_offset = t - offset
+        if t_offset < 0:
+            return self.smaller_0(theta, t_offset)
+        elif t_offset.to_value(u.h) < 0.20734:
+            return (self.smaller_t_0(theta) ** -2 + self.smaller_t_s(theta, t_offset) ** -2) ** -0.5
+        else:
+            return self.smaller_t_s(theta, t_offset) + self.smaller_t_rec(theta, t_offset)
+
+    def obs(self, theta, t) -> float:
+        """eq 5"""
+        offset = theta[3]
+        t_offset = t - (offset * u.d)
+        if t <= 0:
+            return self.smaller_0(theta, t_offset)
+
+        elif t <= self.t_0(theta):
+            return self.smaller_t_0(theta)
+
+        elif self.t_0(theta) <= t <= self.t_s(theta):
+            return self.smaller_t_s(theta, t_offset)
+
+        elif self.t_s(theta) <= t <= self.t_rec(theta):
+            return self.smaller_t_rec(theta, t_offset)
+        return 0
+
+    def smaller_t_0(self, theta) -> float:
+        return 1.8 * 10 ** 45 * self.multi(theta, -0.65, -0.11, 1.37)
+
+    def smaller_0(self, theta, t) -> float:
+        ratio = t / self.t_0(theta)
+        return self.smaller_t_0(theta) * np.exp((-0.35 * ratio ** 2) + (0.15 * ratio))
+
+    def smaller_t_s(self, theta, t) -> float:
+        return self.smaller_t_0(theta) * (t / self.t_0(theta)) ** (-4 / 3)
+
+    def smaller_t_rec(self, theta, t) -> float:
+        return self.smaller_t_0(theta) * (self.t_s(theta) / self.t_0(theta)) ** (-4 / 3) * (t / self.t_s(theta)) ** (
+            -0.35)
+
+
+class FilteredL(L):
+    """not exactly ccording to SOLID, but i need a few things in this calss to make it work properly"""
+    def __init__(self, steps, band_filter='V', system='VegaMag'):
+        super().__init__(steps)
+        self.wave = Wave(band_filter, system)
+        self.system = system
+        self.band_filter = band_filter
+
+    def get_filtered_L(self, theta, t):
+        """the eq after eq 5 in Kozyreva"""
+        temp_limit = (h * self.wave.central_freq) / (3 * k * self.T.col(theta, t, self.wave.central_freq))
+        if temp_limit < 0.3:
+            return self.Rayleign_Jeans(theta, t)
+
+        elif 0.3 < temp_limit < 3:
+            freq_range = self.wave.get_range_freq(self.steps)
+            return np.trapz(y=[self.eq_2(theta, t, nu) for nu in freq_range],
+                            x=freq_range)
+        return 1
+
+    def Rayleign_Jeans(self, theta, t):
+        """
+        the flux of a black body radiation
+        """
+        return self.mag_to_flux(self.blackbody_mag(theta, t))
+
+    def blackbody_mag(self, theta, t):
+        bb = S.BlackBody(self.T.col(theta, t, nu=self.wave.central_freq))
+
+        obs = S.Observation(bb, self.wave.ob)
+
+        mag = obs.effstim(self.system)
+
+        return mag - 2.5 * np.log10(((self.R(theta, t) / (1 * u.solRad)) ** 2) * ((1000.0 * u.pc / distance) ** 2))
+
+    def R(self, theta, t):
+        """
+        gets t in days, then converts the result to rsun
+        R^2 = sqrt(L(t)/4pi sigma T^4)
+        """
+        sigma_sb = const.sigma_sb.to(u.erg * u.cm ** -2 * u.s ** -1 * u.K ** -4)
+        return np.sqrt((self.obs(theta, t) * (u.erg / u.s) / (4 * np.pi * sigma_sb *
+                                                              (self.T.obs(theta, t) * u.K) ** 4))).to(u.solRad)
+    @staticmethod
+    def mag_to_flux(mag):
+        """
+        defenition of mag
+        """
+        return 10 ** (-0.4 * mag)
+
+    def luminosity_to_mag(self, L):
+        """
+        gets the absolute magnitude of a star, given L [erg/s], m_AB - m_Vega = 0.02
+        returns the system in question
+        """
+        mag = -2.5 * np.log10(L / const.L_bol0.to_value(u.erg / u.s))
+        if self.system == 'VegaMag':
+            return mag - 0.02
+        return mag
+
+
+    def eq_2(self, theta, t, nu):
+        """eq 2 in Kozyreva paper, after light travel time, nu is freq"""
+        return 0.9 * self.light_travel_time(theta, t) * 15 / np.pi ** 4 * \
+               (h / (k * self.T.col(theta, t, nu))) ** 4 * nu ** 3 * \
+               (np.exp((h * nu) / (k * self.T.col(theta, t, nu))) - 1) ** -1
+
+
+
+
+
---
+++ starting with data/SWN16-Sushaman/test_figs_K.py
@@ -0,0 +1,78 @@
+import matplotlib.pyplot as plt
+from L_T_R import *
+
+"""this file is for testing the figures from Kozyreva and a bit from Shusman"""
+
+M_ej = 9.3 * u.solMass
+R = 624 * u.solRad
+E_exp = 10**51 * u.erg
+theta = [(R/500).value, (M_ej/15).value, (E_exp/(10**51)).value, 0]
+
+m12l15rot2 = [812/500, 8.09/15, 1.1, 0]
+m12l5rot2 = [345/500, 9.15/15, 1.1, 0]
+m15l15rot0 = [1024/500, 11.33/15, 1.1, 0]
+m15l5rot8 = [268/500, 6.1/15, 1.1, 0]
+
+progenitors = {
+    'm12l15rot2': m12l15rot2,
+    'm12l5rot2': m12l5rot2,
+    'm15l15rot0': m15l15rot0,
+    'm15l5rot8': m15l5rot8
+}
+
+def test_classes():
+    Lumi = L(100)
+    data = pd.read_csv(
+        r"C:\Users\User\OneDrive - mail.tau.ac.il\Desktop\אוניברסיטה\אסטרו נודר\פרויקט קיץ\התחלה של קוד\astro_summer_project\starting with data\SWN16-Sushaman\test_data\noraml_eta.csv")
+    # print(data.loc[:, 'L_bol[erg/s]'])
+    # print([L_obs(theta, (t*u.s).to(u.d)) for t in data.loc[:, 't[s]']])
+    # print(type(L_obs_new(theta, (data.loc[0, 't[s]']*u.s).to(u.d)).value))
+    plt.plot(np.log10(data.loc[:, 't[s]']), np.array(data.loc[:, 'L_bol[erg/s]']), 'r-', label='Test func')
+    # plt.plot(np.log10(data.loc[:, 't[s]']), np.array([float(L_obs_new(theta, (t*u.s).to(u.d))) for t in data.loc[:, 't[s]']]), label='eq 5')
+    plt.plot(np.log10(data.loc[:, 't[s]']),
+             np.array([float(Lumi.broken_power_law(theta, (t * u.s).to(u.d))) for t in data.loc[:, 't[s]']]), label='broken law eq 7')
+    plt.legend()
+    plt.grid()
+    # plt.xlim(-1, 100)
+    plt.ylabel('L[erg/s]')
+    plt.xlabel('log(t[s])')
+    plt.title('R=624 solRad, M=9.3 solMass, E=1e51 erg')
+    plt.show()
+
+
+def test_fig_3():
+    L_ltt = L(100).light_travel_time
+    figure, axis = plt.subplots(2, 2)
+    prog = list(progenitors.items())
+    index = 0
+    time = np.linspace(1, 6)
+    for row in range(2):
+        for cul in range(2):
+            axis[row, cul].plot(time, [np.log10(L_ltt(prog[index][1], (t * u.s).to(u.d))) for t in 10**time])
+            axis[row, cul].set_title(prog[index][0])
+            axis[row, cul].grid()
+            axis[row, cul].set_ylim(42, 46)
+            index += 1
+    plt.show()
+
+def test_fig_5():
+    log_nu = np.linspace(13, 18)
+    L_nu = FilteredL(100).eq_2
+    plt.plot(log_nu, [np.log10(L_nu(m12l15rot2, (300*u.s).to(u.d), nu)) for nu in 10**log_nu])
+    plt.grid()
+    plt.ylim(25, 29)
+    plt.show()
+
+def test_fig_9():
+    log_t = np.linspace(1.5, 6.5)
+    to_mag = FilteredL(100).luminosity_to_mag
+    L_filtered = FilteredL(100).get_filtered_L
+    plt.plot(log_t, [to_mag(L_filtered(m12l15rot2, (t*u.s).to(u.d))) for t in 10**log_t])
+    plt.show()
+
+if __name__ == '__main__':
+    # just_test()
+    # test_web_L_llt()
+    # test_classes()
+    # test_web_L()
+    test_fig_9()
