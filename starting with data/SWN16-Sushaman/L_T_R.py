i switched to simpsons from trapoiz. and calculated the derivative to find the number of steps i should do. one tiny problem with the dimentions i need to fix. other than that it's great. next time i will make it work and continue with the testing. so far it's the right choise.

starting with data/SWN16-Sushaman/L_T_R.py
@@ -2,6 +2,8 @@ from astropy import units as u
 from astropy import constants as const
 import numpy as np
 import pysynphot as S
+import scipy.integrate
+import scipy.misc
 
 h = const.h.to_value(u.erg / u.Hz)
 k = const.k_B.to_value(u.erg / u.K)
@@ -67,7 +69,7 @@ class T(Time):
         elif self.t_c(theta) <= t < self.t_rec(theta):
             return self.T_smaller_t_rec(theta, t)
 
-        return 1
+        return 1.0
 
     def T_smaller_t_0(self, theta):
         return 4.3 * 10 ** 5 * self.multi(theta, -0.17, -0.52, 0.35)
@@ -76,12 +78,15 @@ class T(Time):
         return float(self.T_smaller_t_0(theta) * (t / self.t_0(theta)) ** (-0.45))
 
     def T_smaller_t_c(self, theta, t):
-        return self.T_smaller_t_0(theta) * (self.t_s(theta) / self.t_0(theta)) ** (-0.45) * (t / self.t_s(theta)) ** (-0.35)
+        return float(
+            self.T_smaller_t_0(theta) * (self.t_s(theta) / self.t_0(theta)) ** (-0.45) * (t / self.t_s(theta)) ** (
+                -0.35))
 
     def T_smaller_t_rec(self, theta, t):
-        return self.T_smaller_t_0(theta) * (self.t_s(theta) / self.t_0(theta)) ** (-0.45) * (self.t_c(theta) /
-                                                                                             self.t_s(theta)) ** (-0.35) * \
-                   (t / self.t_c(theta)) ** (-0.6)
+        return float(self.T_smaller_t_0(theta) * (self.t_s(theta) / self.t_0(theta)) ** (-0.45) * (self.t_c(theta) /
+                                                                                                   self.t_s(theta)) ** (
+                         -0.35) * \
+                     (t / self.t_c(theta)) ** (-0.6))
 
 
 class Wave:
@@ -106,7 +111,6 @@ class Wave:
         """
         return (const.c.value / frequency * u.m).to_value(u.angstrom)
 
-
     def get_range_freq(self):
         """
         return the frequency by the self.ob.wave object
@@ -114,23 +118,48 @@ class Wave:
         return np.flip(self.length_to_frequency(self.ob.wave))
 
 
-class L(Time):
-    def __init__(self, steps=100, band_filter='V', system='VegaMag'):
+class Integrator:
+    """
+    integral approxiamtor, according to the following webpages:
+    https://www.whitman.edu/mathematics/calculus_online/section08.06.html
+    https://en.wikipedia.org/wiki/Five-point_stencil
+    all the names are trying to be similar to these pages.
+    Using the default value for linspace
+    """
+
+    def __init__(self, error=0.001):
+        self.error = error
+
+    def get_steps(self, func, a, b, *args):
+        return self.make_even(round(((b - a) ** 5 * self.get_M(func, a, b, args) / (180 * self.error)) ** (1 / 4)) + 1)
+
+    @staticmethod
+    def make_even(n):
+        """it is one of the demand for simpson's rule"""
+        return n // 2 * 2
+
+    def get_M(self, func, a, b, *args):
+        return max(scipy.misc.derivative(func, x, dx=10 ** -2, n=4, order=5, args=args) for x in np.linspace(a, b))
+
+
+class L(Time, Integrator):
+    def __init__(self, error=0.001, band_filter='V', system='VegaMag'):
+        super().__init__(error)
         self.T = T()
-        self.steps = steps
         self.wave = Wave(band_filter, system)
         self.system = system
         self.band_filter = band_filter
 
     def light_travel_time(self, theta, t, L_function) -> float:
         """eq 1 in Kozyreva paper"""
-        dx = self.t_rc(theta) / self.steps
+        n = self.get_steps(self.ltt_integrant, t - self.t_rc(theta), t, (theta, t, L_function))
+        dx = self.t_rc(theta) / n
         return 2 / self.t_rc(theta) * \
-               np.trapz([self.ltt_integrant(theta, t, t_tag, L_function) for t_tag in
-                         np.linspace(t - self.t_rc(theta), t, self.steps)],
-                        dx=dx)
+               scipy.integrate.simpson([self.ltt_integrant(t_tag, theta, t, L_function) for t_tag in
+                                        np.linspace(t - self.t_rc(theta), t, n + 1)],
+                                       dx=dx)
 
-    def ltt_integrant(self, theta, t, t_tag, L_function) -> float:
+    def ltt_integrant(self, t_tag, theta, t, L_function) -> float:
         """the function inside the integral"""
         return L_function(theta, t_tag) * (1 - (t - t_tag) / self.t_rc(theta))
 
@@ -162,22 +191,22 @@ class L(Time):
 
         elif self.t_s(theta) <= t <= self.t_rec(theta):
             return self.smaller_t_rec(theta, t)
-        return 1
+        return 1.0
 
     def smaller_t_0(self, theta) -> float:
         return 1.8 * 10 ** 45 * self.multi(theta, -0.65, -0.11, 1.37)
 
     def smaller_0(self, theta, t) -> float:
         ratio = t / self.t_0(theta)
-        return self.smaller_t_0(theta) * np.exp((-0.35 * ratio ** 2) + (0.15 * ratio))
+        return float(self.smaller_t_0(theta) * np.exp((-0.35 * ratio ** 2) + (0.15 * ratio)))
 
     def smaller_t_s(self, theta, t) -> float:
-        return self.smaller_t_0(theta) * (t / self.t_0(theta)) ** (-4 / 3)
+        return float(self.smaller_t_0(theta) * (t / self.t_0(theta)) ** (-4 / 3))
 
     def smaller_t_rec(self, theta, t) -> float:
-        return (self.smaller_t_0(theta) * (self.t_s(theta) / self.t_0(theta)) ** (-4 / 3) * (t / self.t_s(theta)) ** (
-            -0.35)).to_value()
-
+        return float(
+            self.smaller_t_0(theta) * (self.t_s(theta) / self.t_0(theta)) ** (-4 / 3) * (t / self.t_s(theta)) ** (
+                -0.35))
 
 
 class Magnitude(L):
@@ -186,7 +215,7 @@ class Magnitude(L):
     def get_filtered_abs_mag(self, theta, t):
         """the eq after eq 5 in Kozyreva"""
         offset = theta[3]
-        t_offset = t - offset
+        t_offset = (t - offset) * u.d
         temp_limit = (h * self.wave.central_freq) / (3 * k * self.T.col(theta, t_offset, self.wave.central_freq))
         use_func = None
         if temp_limit < 0.3:
@@ -204,7 +233,7 @@ class Magnitude(L):
     def absolute_mag_filtered(self, theta, t):
         freq_range = self.wave.get_range_freq()
         flux = np.array([self.eq_2(theta, t, nu) for nu in freq_range]) / (
-                    4 * np.pi * self.R(theta, t).to_value(u.cm) ** 2)
+                4 * np.pi * self.R(theta, t).to_value(u.cm) ** 2)
         wave_range = self.wave.frequency_to_length(freq_range)
 
         # flipped them so it would be from small to large
@@ -235,11 +264,8 @@ class Magnitude(L):
         for this one i need the bolometric luminosity! so i will use broken power law with ltt
         """
         sigma_sb = const.sigma_sb.to(u.erg * u.cm ** -2 * u.s ** -1 * u.K ** -4)
-        return np.sqrt(
-            (self.light_travel_time(theta, t, self.broken_power_law) * (u.erg / u.s) / (4 * np.pi * sigma_sb *
-                                                                                        (self.T.obs(theta,
-                                                                                                    t) * u.K) ** 4))).to(
-            u.solRad)
+        return np.sqrt((self.light_travel_time(theta, t, self.broken_power_law) *
+                        (u.erg / u.s) / (4 * np.pi * sigma_sb * (self.T.obs(theta, t) * u.K) ** 4))).to(u.solRad)
 
     @staticmethod
     def to_pseudo_flux_from_mag(mag):
@@ -262,7 +288,6 @@ class Magnitude(L):
         a bit of derivation of errors, check with iair if this is the right way!!
         """
         return abs(self.to_pseudo_flux_from_mag(mag) * (-0.4) * np.log(10) * mag_err)
-
 
 
 class FilteredL(L):

starting with data/SWN16-Sushaman/test_LTR_unittest.py
@@ -3,6 +3,8 @@ from L_T_R import *
 from astropy import units as u
 
 
+theta = [812 / 500, 8.09 / 15, 1.1, 0]
+
 class TestWave(unittest.TestCase):
     def setUp(self) -> None:
         self.V = Wave()
@@ -59,16 +61,114 @@ class TestT(unittest.TestCase):
     def test_T_smaller_t_s(self):
         self.assertEqual(type(self.T.T_smaller_t_s(self.theta, self.t)), float)
 
+    def test_T_smaller_t_c(self):
+        self.assertEqual(type(self.T.T_smaller_t_c(self.theta, self.t)), float)
+
+    def test_T_smaller_t_rec(self):
+        self.assertEqual(type(self.T.T_smaller_t_rec(self.theta, self.t)), float)
+
+    def test_T_obs(self):
+        times = [0 * u.d, self.T.t_0(self.theta), self.T.t_s(self.theta), self.T.t_c(self.theta),
+                 self.T.t_rec(self.theta)]
+        for time in times:
+            self.assertEqual(type(self.T.obs(self.theta, time)), float)
+
+    def test_T_col(self):
+        times = [0 * u.d, self.T.t_0(self.theta), self.T.t_s(self.theta), self.T.t_c(self.theta),
+                 self.T.t_rec(self.theta)]
+        for time in times:
+            self.assertEqual(type(self.T.col(self.theta, time, nu=5.4601e+14)), np.float64)
 
 
 class TestL(unittest.TestCase):
     def setUp(self) -> None:
         self.L = L()
         self.theta = [812/500, 8.09/15, 1.1, 0]
-        self.t = 1
+        self.t = 1 * u.d
+        self.times = [0*u.d, self.L.t_0(self.theta), self.L.t_s(self.theta), self.L.t_rec(self.theta)]
+        self.nu = 5.4601e+14
 
     def test_smaller_t_rec(self):
-        self.assertEqual(type(self.L.smaller_t_rec(theta=self.theta, t=self.t)), np.float64)
+        self.assertEqual(type(self.L.smaller_t_rec(theta=self.theta, t=self.t)), float)
+
+    def test_smaller_t_s(self):
+        self.assertEqual(type(self.L.smaller_t_s(theta=self.theta, t=self.t)), float)
+
+    def test_smaller_0(self):
+        self.assertEqual(type(self.L.smaller_0(theta=self.theta, t=self.t)), float)
+
+    def test_smaller_t_0(self):
+        self.assertEqual(type(self.L.smaller_t_0(theta=self.theta)), float)
+
+    def test_obs(self):
+        for time in self.times:
+            self.assertEqual(type(self.L.obs(self.theta, time)), float)
+
+    def test_eq2(self):
+        for time in self.times:
+            self.assertEqual(type(self.L.eq_2(self.theta, time, self.nu)), np.float64)
+
+    """still need to test the rest of the functions"""
+    # def test_integrant(self):
+    #     for i, time in enumerate(self.times):
+    #         if i == len(self.times) - 1:
+    #             break
+    #         for t_tag in np.linspace(time, self.times[i+1]):
+    #             self.assertEqual(type(self.L.ltt_integrant(self.theta, time, t_tag, L.eq_2)), np.float64)
+
+
+class TestMagnitude(unittest.TestCase):
+    def setUp(self) -> None:
+        self.mag = Magnitude()
+        self.theta = [812 / 500, 8.09 / 15, 1.1, 0]
+        self.times = [0 * u.d, self.mag.t_0(self.theta), self.mag.t_s(self.theta), self.mag.t_rec(self.theta)]
+        self.t = 1 * u.d
+
+    def test_apparent_to_absolute(self):
+        for absolute_mag in range(-15, 15):
+            self.assertEqual(self.mag.convert_apparent_to_absolute(
+                self.mag.convert_absolute_to_apparent(absolute_mag, distance),distance), absolute_mag)
+
+    def test_pseudo_flux_to_mag(self):
+        for absolute_mag in range(-15, 15):
+            self.assertAlmostEqual(self.mag.to_mag_from_pseudo_flux(
+                self.mag.to_pseudo_flux_from_mag(absolute_mag)), absolute_mag)
+
+    def test_pseudo_fluxerr(self):
+        for absolute_mag in range(-15, 15):
+            for mag_error in np.linspace(0.01, 1):
+                self.assertGreaterEqual(self.mag.absolute_mag_to_fluxerr(absolute_mag, mag_error), 0)
+
+    # def test_R(self):
+    #     for time in self.times:
+    #         print(self.mag.R(self.theta, time))
+
+class TestIntegrator(unittest.TestCase):
+    def setUp(self) -> None:
+        self.integrator = Integrator()
+        self.func_example = lambda x: np.exp(-(x ** 2))
+        self.mag = Magnitude()
+        self.a = 0
+        self.b = 1
+        self.theta = [812 / 500, 8.09 / 15, 1.1, 0]
+        self.error = 0.001
+
+    def test_get_M(self):
+        self.assertTrue(11.5 < self.integrator.get_M(self.func_example, self.a, self.b) < 12)
+
+    def test_get_steps(self):
+        self.assertAlmostEqual(self.integrator.get_steps(self.error, self.func_example, self.a, self.b), 4)
+
+    def test_make_enev(self):
+        for n in range(20):
+            self.assertTrue(self.integrator.make_even(n) % 2 == 0)
+
+    def test_actual_func(self):
+        t = (self.mag.t_0(theta) + self.mag.t_s(theta)) / 2
+        print(self.integrator.get_steps(L.ltt_integrant, t - self.mag.t_rc(theta), t,
+                                        (theta, t, self.mag.absolute_filtered_pseudo_flux)))
+
+
 
 
 